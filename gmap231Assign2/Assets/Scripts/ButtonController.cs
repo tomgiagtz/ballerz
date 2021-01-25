0982e5a8b889910fda15fd9b7bd895c335998ec2
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2021-01-25 14:34:36 -0500

    Add button animation. create buttoncontroller

---
+++ gmap231Assign2/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public bool isPressed = false;
+    public float pressSpeed = 1f;
+    [Range (0, 100)] public float pressRatio = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
